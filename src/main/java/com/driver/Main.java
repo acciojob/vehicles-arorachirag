87c4d784c279ce977e0dcde5e402fe463dd17e67
arorachirag <arorachirag52@gmail.com>
2023-11-18 12:24:49 +0530

    Code changes 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
+
+        Boat b1 = new Boat("Cruise",1000);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
     }
