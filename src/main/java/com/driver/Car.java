647764d5580e6e67510c49f10348a18f9292a639
arorachirag <arorachirag52@gmail.com>
2023-11-18 12:43:46 +0530

    Code changes 3

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -79,7 +79,7 @@ public class Car extends Vehicle {
 
     public void changeGear(int newGear){
         this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+        System.out.println("changeGear method called - The gear is changed to: " + this.currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
